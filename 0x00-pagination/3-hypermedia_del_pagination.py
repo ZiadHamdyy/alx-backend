e7a92fef479ee6f9a69056181e5e3171ba9ed699
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-07 00:07:47 +0300

    task 3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -3,6 +3,8 @@ from typing import Dict
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
