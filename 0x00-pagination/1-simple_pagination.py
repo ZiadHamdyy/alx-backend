f2f4479ff50c6dd4151cdd98506df718d87ec0ee
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-06 23:44:11 +0300

    task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -25,21 +25,25 @@ class Server:
 
         return self.__dataset
 
+    @staticmethod
+    def index_range(page: int, page_size: int) -> tuple:
+        """return a tuple of size two containing a start
+        index and an end index"""
+        start_index = (page - 1) * page_size
+        end_index = page * page_size
+        return (start_index, end_index)
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer"
-    
+        """return the appropriate page of the dataset"""
+        assert isinstance(page, int) and page > 0, \
+            "Page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size must be a positive integer"
+
         dataset = self.dataset()
         total_rows = len(dataset)
         if total_rows == 0 or (page - 1) * page_size >= total_rows:
             return []
-    
-        start_index, end_index = index_range(page, page_size)
+
+        start_index, end_index = self.index_range(page, page_size)
         return dataset[start_index:end_index]
-
-
-def index_range(page: int, page_size: int) -> tuple:
-    """return a tuple of size two containing a start index and an end index"""
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return (start_index, end_index)
