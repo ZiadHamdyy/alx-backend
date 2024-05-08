5a8f96fa7214085e15db343a476a8fa7362454a9
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-08 13:22:15 +0300

    task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-""" 1-fifo_cache.py """
+""" 2-lifo_cache.py """
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """BasicCache that inherits from BaseCaching and is a caching system"""
 
     def __init__(self):
-        """ Initializes the FIFO cache """
+        """ Initializes the LIFO cache """
         super().__init__()
-        self.queue = []
+        self.stack = []
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discarded_key = self.queue.pop(0)
+                discarded_key = self.stack.pop()
                 del self.cache_data[discarded_key]
                 print("DISCARD:", discarded_key)
             self.cache_data[key] = item
-            self.queue.append(key)
+            self.stack.append(key)
 
     def get(self, key):
         """ Get an item by key """
